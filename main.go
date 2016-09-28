1966b8f79ae076ba8d128595f88494f31a4120f1
Rohan Allison <rohanthewiz_at_gmail.com>
2016-09-28 10:30:28 -0500

    Use a less popular port: 8094

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	product.InitDB()
 	defer product.CloseDB()
 
-	log.Println("Starting the server at http://localhost:8080...")
-	err := fasthttp.ListenAndServe("localhost:8080", hash_router.Route)
+	log.Println("Starting the server at http://0.0.0.0:8094...")
+	err := fasthttp.ListenAndServe("0.0.0.0:8094", hash_router.Route)
 	if err != nil {
 		log.Fatal("unexpected error in server:", err)
 	}
